03da343c7372f0c6656dad3048bf26ec20052f67
ctlandry <clandry1982@gmail.com>
2019-08-10 10:57:36 -0700

    Initial Commit

---
+++ src/xam.native.core/Droid/Fragments/IFragmentVisible.cs
@@ -0,0 +1,7 @@
+﻿namespace xam.native.core.Droid
+{
+    interface IFragmentVisible
+    {
+        void BecameVisible();
+    }
+}
---
+++ src/xam.native.core/Droid/Helpers/RecyclerClickEvent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Android.Views;
+
+namespace xam.native.core.Droid
+{
+    public class RecyclerClickEventArgs : EventArgs
+    {
+        public View View { get; set; }
+        public int Position { get; set; }
+    }
+}
