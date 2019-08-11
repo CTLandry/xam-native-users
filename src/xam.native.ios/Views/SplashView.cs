dd61fb3884f5542214f8f99511be2d6a8bfc9277
ctlandry <clandry1982@gmail.com>
2019-08-10 23:04:48 -0700

    rebuild of iOS, needs debugging

---
+++ src/xam.native.ios/Views/SplashView.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using MvvmCross.Binding.BindingContext;
+using MvvmCross.Platforms.Ios.Views;
+using UIKit;
+using xam.native.core.ViewModels;
+
+namespace xam.native.ios.Views
+{
+    public partial class SplashView : MvxViewController<SplashViewModel>
+    {
+        public SplashView() : base("SplashView", null)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            var set = this.CreateBindingSet<SplashView, SplashViewModel>();
+            
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
+
